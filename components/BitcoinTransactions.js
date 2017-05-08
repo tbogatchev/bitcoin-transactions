ec952255a92eac4e888798ab2b0ec5f97713c990
Tim Bogatchev <tim@ticketevolution.com>
2017-05-08 13:10:47 -0400

    everything works, just need to style

--- components/BitcoinTransactions.js
+++ components/BitcoinTransactions.js
@@ -1,14 +1,89 @@
 import React from 'react';
+import Transaction from './Transaction';
+
+const MAX_TRANSACTIONS = 15;
+let socket;
 
 export default class BitcoinTransactions extends React.Component {
+
+  constructor (props) {
+    super(props);
+    this.state = {
+      transactions: [{value: 75621712, address: 12323412341234}, {value: 75621712, address: 12323412341234}, {value: 75621712, address: 12323412341234}, {value: 75621712, address: 12323412341234}],
+      total: 2398477213,
+      totalTransactions: 4
+    };
+  }
+
+  componentDidMount() {
+    socket = new WebSocket('wss://ws.blockchain.info/inv');
+
+    socket.onopen = function () {
+      socket.send(JSON.stringify({ op: "unconfirmed_sub"}));
+    };
+    socket.onmessage = this.handleSocketMessage.bind(this);
+  }
+
+  componentWillUnmount() {
+    socket.close();
+  }
+
+  handleSocketMessage (msg) {
+    let {transactions, total, totalTransactions} = this.state;
+    const data = JSON.parse(msg.data);
+
+    let newTransactions = [];
+    if (data && data.x && data.x.out){
+      newTransactions = data.x.out.map((newT) => {
+        total += newT.value;
+        totalTransactions += 1;
+        return {
+          value: newT.value,
+          address: newT.addr
+        }
+      })
+    }
+
+    while (transactions.length + newTransactions.length > MAX_TRANSACTIONS){
+      transactions.shift();
+    }
+
+    this.setState({
+      total: total,
+      totalTransactions: totalTransactions,
+      transactions: transactions.concat(newTransactions)
+    });
+  }
+
   render() {
+    const formatter = new Intl.NumberFormat('en-US', {
+      style: 'currency',
+      currency: 'USD',
+      minimumFractionDigits: 2,
+    });
+
     return (
       <div>
-        <ul id="messages"></ul>
-        <form action="">
-          <input id="m" autoComplete="off" /><button>Send</button>
-        </form>
+        <div className="title">Bitcoin Transaction Stream</div>
+        <div className="streamContainer container">
+          <div className="transactionsContainer col">
+            {this.state.transactions.map(transaction =>
+              <Transaction value={ formatter.format(transaction.value) } address={ transaction.address }/>
+            )}
+          </div>
+          <div className="container col">
+            <div className="col">
+              <div>Total Value</div>
+              <div>Total Transactions</div>
+            </div>
+            <div className = "col">
+              <div>{formatter.format(this.state.total)}</div>
+              <div>{this.state.totalTransactions}</div>
+            </div>
+          </div>
+        </div>
       </div>
+
     );
   }
 }
---
+++ components/Transaction.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+export default class Transaction extends React.Component {
+  render() {
+    const {value, address} = this.props;
+    return (
+      <div className="container">
+        <div className="col">
+          <div>Value</div>
+          <div>Address</div>
+        </div>
+        <div className="col">
+          <div>{value}</div>
+          <div>{address}</div>
+        </div>
+      </div>
+    );
+  }
+}
